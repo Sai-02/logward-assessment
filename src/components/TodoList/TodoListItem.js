88a4d1a565bb412481e08a7534b1da009198e7b5
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2023-02-24 16:19:00 +0530

    delete icon added

--- src/components/TodoList/TodoListItem.js
+++ src/components/TodoList/TodoListItem.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { TaskContext } from "../../App";
 import DeleteTaskModal from "./DeleteTaskModal";
-
+import { Delete } from "@mui/icons-material";
 const TodoListItem = ({ task }) => {
   const { tasksObj, setTasksObj, activeList } = useContext(TaskContext);
   const [shouldOpenDeleteTask, setShouldOpenDeleteTask] = useState(false);
@@ -22,12 +22,20 @@ const TodoListItem = ({ task }) => {
 
   return (
     <div className="flex justify-between p-6 shadow items-center">
-      <input type="checkbox" value={task.completed} onClick={changeStatus} className="w-4 h-4" />
+      <input
+        type="checkbox"
+        value={task.completed}
+        onClick={changeStatus}
+        className="w-4 h-4"
+      />
       <h3 className="text-lg">
         {task.completed ? <s>{task.name}</s> : <>{task.name}</>}
       </h3>
-      <button className="text-[red]" onClick={openDeleteTask}>
-        Delete
+      <button
+        className="text-[red] rounded-full hover:bg-[#00000024] p-1"
+        onClick={openDeleteTask}
+      >
+        <Delete />
       </button>
       <DeleteTaskModal
         task={task}
